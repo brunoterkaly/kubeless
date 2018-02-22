29706aae4842cdecc4bafbcf10d35056e47701a5
root <root@bt-ubuntu-vm.13lqoqvbrnsere4xgq5u4hgb5d.xx.internal.cloudapp.net>
2018-02-22 00:35:42 +0000

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # kubeless
 # kubeless
 # kubeless
+# kubeless
--- check-in.sh
+++
@@ -1,9 +0,0 @@
-echo "# kubeless" >> README.md
-git init
-git add README.md
-git add *.sh
-git remove --cached checkin*.sh
-git add *.yaml
-git commit -m "first commit"
-git remote add origin https://github.com/brunoterkaly/kubeless.git
-git push -u origin master
