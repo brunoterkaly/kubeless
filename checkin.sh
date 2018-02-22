7e7cd94c8ab4b032f64c7ced8c563dc3d90e15d5
root <root@bt-ubuntu-vm.13lqoqvbrnsere4xgq5u4hgb5d.xx.internal.cloudapp.net>
2018-02-22 00:01:37 +0000

    first commit

--- checkin.sh
+++ checkin.sh
@@ -3,6 +3,7 @@ git init
 git add README.md
 git add *.sh
 git add *.yaml
+git rm checkin.sh
 git commit -m "first commit"
 git remote add origin https://github.com/brunoterkaly/kubeless.git
 git push -u origin master
